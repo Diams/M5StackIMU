7f84d75dc27f4e8df0d25fdd6ce601d46f91c320
Masanao Daimon <diams.rider@gmail.com>
2023-03-08 22:07:01 +0900

    Fix format of acceleration sensor.

--- m5stack_imu/m5stack_imu.ino
+++ m5stack_imu/m5stack_imu.ino
@@ -10,7 +10,7 @@ void loop(void) {
   float ax, ay, az;
   M5.Imu.getAccelData(&ax, &ay, &az);
   M5.Lcd.setCursor(0, 0);
-  M5.Lcd.printf("acc:% 3.2f, % 3.2f, % 3.2f", ax, ay, az);
+  M5.Lcd.printf("acc:% 3.2f,% 3.2f,% 3.2f", ax, ay, az);
   float gx, gy, gz;
   M5.Imu.getGyroData(&gx, &gy, &gz);
   M5.Lcd.setCursor(0, 50);
