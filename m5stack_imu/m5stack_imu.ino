fdf18cd71ef1e83407544ae7819aeec47f4b3f9b
Masanao Daimon <diams.rider@gmail.com>
2023-03-08 22:03:01 +0900

    Update to get values of gyro sensor.

--- m5stack_imu/m5stack_imu.ino
+++ m5stack_imu/m5stack_imu.ino
@@ -11,5 +11,9 @@ void loop(void) {
   M5.Imu.getAccelData(&ax, &ay, &az);
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.printf("acc:% 3.2f, % 3.2f, % 3.2f", ax, ay, az);
+  float gx, gy, gz;
+  M5.Imu.getGyroData(&gx, &gy, &gz);
+  M5.Lcd.setCursor(0, 50);
+  M5.Lcd.printf("gyro:% 4d,% 4d,% 4d", (int)gx, (int)gy, (int)gz);
   delay(1000);
 }
